17a7700667b1c9d0382efc0b019f239f0aa91bb0
Igor Kirpinev <ikirpinev@gmail.com>
2020-03-14 13:42:05 +0300

    Конфигурация токена вынесена в отдельный файл

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { verifyToken } = require('../utils/token');
 
 module.exports.auth = (req, res, next) => {
   const { jwt: token } = req.cookies;
@@ -12,12 +11,9 @@ module.exports.auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'secret-key'
-    );
+    payload = verifyToken(token);
   } catch (err) {
-    return res.status(401).send({ message: ' Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload;
