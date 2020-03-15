1b28d70d880bd923bb46d4625deedb4868de11bb
Igor Kirpinev <ikirpinev@gmail.com>
2020-03-15 23:16:23 +0300

    Убрал фигурные скобки и return в функциях создания и проверки токена

--- utils/token.js
+++ utils/token.js
@@ -4,10 +4,6 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 
-module.exports.createToken = user => {
-  return jwt.sign({ _id: user._id }, secretKey);
-};
+module.exports.createToken = user => jwt.sign({ _id: user._id }, secretKey);
 
-module.exports.verifyToken = token => {
-  return jwt.verify(token, secretKey);
-};
+module.exports.verifyToken = token => jwt.verify(token, secretKey);
