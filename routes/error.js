b4dd7714d939d01b752c846f2c7f9edac7beff29
Igor Kirpinev <ikirpinev@gmail.com>
2020-03-29 15:50:18 +0300

    Рефакторинг

--- routes/error.js
+++ routes/error.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { messages } = require('../utils/messages');
 
 router.all('*', (req, res) => {
-  res.status(404).send({ status: 404, message: messages.root.isNotFound });
+  res.status(404).send({ message: messages.root.isNotFound });
 });
 
 module.exports = router;
