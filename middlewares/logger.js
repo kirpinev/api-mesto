9fc0181f94d6b30c41ad0d8922de2874d0c1ece1
Igor Kirpinev <ikirpinev@gmail.com>
2020-04-05 18:04:50 +0300

    Переписал логгер

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,17 +1,12 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-const requestLogger = expressWinston.logger({
+module.exports.requestLogger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: 'request.log' })],
   format: winston.format.json(),
 });
 
-const errorLogger = expressWinston.errorLogger({
+module.exports.errorLogger = expressWinston.errorLogger({
   transports: [new winston.transports.File({ filename: 'error.log' })],
   format: winston.format.json(),
 });
-
-module.exports = {
-  requestLogger,
-  errorLogger,
-};
